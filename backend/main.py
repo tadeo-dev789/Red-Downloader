bd6041b1a99ebdd1cefaa3d360df7406ad8c2d9b
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-12-24 13:08:51 -0600

    Implementé el endpoint para descargar de forma exitosa las canciones

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,42 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+import os
+import yt_dlp
 
 app = FastAPI()
+
+downloads_dir  = Path ("./downloads")
+downloads_dir.mkdir(parents=True, exist_ok=True)
 
 @app.get("/")
 def read_root():
     return{"Hello":"World"}
 
-@app.get("/download/{url}")
+@app.get("/download/")
 def download(url:str):
+    ydl_opts = {
+        'outtmpl': str(downloads_dir / '%(title)s.%(ext)s'),
+        'format' : 'bestaudio/best',
+        'postprocessors':[{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality':'320',
+        }]
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url,download=False)
+        predicted_filename = ydl.prepare_filename(info)
+        error_code = ydl.download(url)
+    
+    mp3_filename = Path(predicted_filename).with_suffix('.mp3')
+    
+    return FileResponse(mp3_filename)
+    
+    
+
     
   
--- backend/test.py
+++ backend/test.py
@@ -3,13 +3,13 @@ import yt_dlp
 URLS = ['https://youtu.be/Kq_plVrBPx4?si=fhnf3nVpMSa3pcg9']
 
 ydl_opts = {
-    'format': 'm4a/bestaudio/best',
+    'format': 'bestaudio/best',
     # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
     'postprocessors': [{  # Extract audio using ffmpeg
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality':'320',
-    }],'outtmpl': '%(title)s.%(etx)s'
+    }]
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
