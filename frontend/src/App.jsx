81428d03434b93059864fd9aa582ed452b7e7d98
tadeo-dev789 <tadeosandovalsa@gmail.com>
2025-12-25 12:58:37 -0600

    "Se avanzó a la creación del diseño del frontend, se agregó un boton de pegar el link de youtube y su respectiva verificación"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  AbsoluteCenter,
+  Button,
+  Box,
+  Input,
+  InputGroup,
+  InputRightElement,
+  IconButton,
+  Stack,
+  Heading
+} from '@chakra-ui/react'
+import { LinkIcon } from '@chakra-ui/icons'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("")
+
+  const handlePaste = async () => {
+    try {
+      const ytRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/;
+
+      const text = await navigator.clipboard.readText()
+      console.log("Clipboard:", text)
+
+      if(ytRegex.test(text))
+        setValue(text)
+      else
+        alert("Ingresa una url valida")
+
+    } catch (err) {
+      console.error("Clipboard error:", err)
+      alert("Clipboard access blocked by browser")
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box
+        position="relative"
+        minH="100vh"
+        bg="red.500"
+      >
+        <AbsoluteCenter axis="both">
+          <Box
+            bg="white"
+            p={5}
+            w="100%"
+            maxW="500px"
+            borderRadius="md"
+            textAlign="center"
+          >
+            <Stack spacing={4}>
+              <Heading as="h1">
+                Red Downloader
+              </Heading>
+
+              <InputGroup size="md">
+                <Input
+                  type="text"
+                  placeholder="Pega el link aquí"
+                  pr="4.5rem"
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                />
+
+                <InputRightElement width="4.5rem">
+                  <IconButton
+                    aria-label="Pegar link"
+                    icon={<LinkIcon />}
+                    h="1.75rem"
+                    size="sm"
+                    colorScheme="gray"
+                    variant="ghost"
+                    onClick={handlePaste}
+                  />
+                </InputRightElement>
+              </InputGroup>
+
+              <Button
+                bg="red.600"
+                color="white"
+              >
+                Descargar
+              </Button>
+            </Stack>
+          </Box>
+        </AbsoluteCenter>
+      </Box>
     </>
   )
 }
